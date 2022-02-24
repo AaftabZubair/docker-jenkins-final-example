62e31e56a7b378a69c3314140b393e577b36d62f
aaftabalam <ZubairJava222@gmail.com>
2022-02-24 09:05:48 +0530

    dockerjenkins

---
+++ src/main/java/com/jenkinsdockerfinalexample/dockerjenkinsfinalexample/DockerJenkinsFinalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsdockerfinalexample.dockerjenkinsfinalexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsFinalExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsFinalExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinsdockerfinalexample/dockerjenkinsfinalexample/DockerJenkinsFinalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsdockerfinalexample.dockerjenkinsfinalexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsFinalExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
